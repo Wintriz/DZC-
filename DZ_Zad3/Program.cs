f10d4525ac290b0c03cebdabf28d2ae1a3cffec6
Andrey <rf5800@mail.ru>
2022-12-17 12:46:23 +0300

    DZ_Zad3

---
+++ DZ_Zad3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 3. Найти максимальное из трех чисел
+int a = 100;
+int b = 120;
+int c = 99;
+int max = 0;
+if (a>max) max = a;
+if (b>max) max = b;
+if (c>max) max = c;
+Console.Write("Максимальное из трех чисел: ");
+Console.Write(max);
