70f764266633b6ae00850a21b3541998185d09cc
Andrey <rf5800@mail.ru>
2022-12-17 13:26:06 +0300

    DZ_Zad2

--- DZ_Zad2/Program.cs
+++ DZ_Zad2/Program.cs
@@ -1,1 +1,46 @@
 ﻿// Задача 2. По заданному номеру дня недели вывести его название
+int n1 = 1;
+int n2 = 2;
+int n3 = 3;
+int n4 = 4;
+int n5 = 5;
+int n6 = 6;
+int n7 = 7;
+Console.WriteLine("Введите число от 1 до 7: ");
+int num = int.Parse(Console.ReadLine()!);
+if (num == n1)
+{
+    Console.WriteLine("Понедельник");
+}
+if (num == n2)
+{
+    Console.WriteLine("Вторник");
+}
+if (num == n3)
+{
+    Console.WriteLine("Среда");
+}
+if (num == n4)
+{
+    Console.WriteLine("Четверг");
+}
+if (num == n5)
+{
+    Console.WriteLine("Пятница");
+}
+if (num == n6)
+{
+    Console.WriteLine("Суббота");
+}
+if (num == n7)
+{
+    Console.WriteLine("Воскресение");
+}
+if (num > n7)
+{
+    Console.WriteLine("Нет такого дня недели, учи матчасть!");
+}
+if (num < n1)
+{
+    Console.WriteLine("Нет такого дня недели, учи матчасть!");
+}
