b18e4150207faefea9f4d2647bc30a3c7249a5d8
Andrey <rf5800@mail.ru>
2022-12-17 12:55:37 +0300

    DZ_Zad4

--- DZ_Zad4/Program.cs
+++ DZ_Zad4/Program.cs
@@ -1,1 +1,11 @@
 ﻿// Задача 4. Выяснить является ли число чётным
+Console.WriteLine("Enter a number: ");
+int num = int.Parse(Console.ReadLine()!);
+if (num % 2 == 0)
+{
+    Console.WriteLine("even");
+}
+else
+{
+    Console.WriteLine("odd");
+}
